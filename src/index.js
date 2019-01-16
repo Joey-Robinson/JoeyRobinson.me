b62bbf63c6f1caafaf16c431540626de4897b247
elamahpla <theamazingness@gmail.com>
2019-01-16 01:43:03 -0600

    This one too

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PersonalSite from './Containers/PersonalSite/PersonalSite';
-import './Styles/styles.scss';
-import 'normalize.css';
-
-ReactDOM.render(<PersonalSite />, document.getElementById('app'));
-
-module.hot.accept();
